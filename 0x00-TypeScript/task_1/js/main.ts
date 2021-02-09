33408dee1fc1a2487a061f81762e9da50697d965
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-09 09:44:37 -0500

    [add] Task 2

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    location: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    [key: string]: any;
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
