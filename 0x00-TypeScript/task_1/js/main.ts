6a1cc64e165508b9c6e4393ecfd3d2d9ddbb1bb6
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-09 11:25:32 -0500

    [add] interface for a function

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -18,16 +18,27 @@ interface Teacher {
 //
 // console.log(teacher3);
 
-//Task 2
+// Task 2
 interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-console.log(director1);
+// const director1: Directors = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     location: 'London',
+//     fullTimeEmployee: true,
+//     numberOfReports: 17,
+// };
+// console.log(director1);
+
+// Task 3
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = function(firstName: string, lastName: string) {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe")) //-> J. Doe
