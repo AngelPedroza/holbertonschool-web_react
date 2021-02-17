75a784b0757fc6dcd9fb4de6d6de91ec3b62cac1
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-17 12:47:35 -0500

    [Add] Task 4 add tests for utils

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.jpg';
+import './App.css';
+import './utils';
+import {getFooterCopy, getFullYear} from "./utils";
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email"/>
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password"/>
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
