125a4ebb11b2c132f9e984861c0adf7b17f975f1
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-16 23:59:54 -0500

    [Add] Task 0

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
