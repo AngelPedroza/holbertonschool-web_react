6525f2461b556a55c941311724c375419f99b11e
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-17 15:57:59 -0500

    [Add] Task 6

---
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,24 @@
+import App from "./App";
+import { shallow } from 'enzyme';
+
+describe("Testing the behavior of App component", () => {
+    it("App renders without crashing", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists());
+    });
+    it("App renders App-header", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find(".App-header")).toHaveLength(1);
+    });
+    it("App renders App-body", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find(".App-body")).toHaveLength(1);
+    });
+    it("App renders App-footer", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find(".App-footer")).toHaveLength(1);
+    });
+});
