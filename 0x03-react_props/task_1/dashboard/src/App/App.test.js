8ab8d42be5da6acb3e280bddbfb099b2c55d0a72
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-23 17:03:59 -0500

    [Add] Task_1 perfect

---
+++ 0x03-react_props/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import App from "./App";
+import { shallow } from 'enzyme';
+
+describe("Testing the behavior of App component", () => {
+    it("App renders without crashing", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists());
+    });
+    it("Should contain the Notifications component", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find("Notifications")).toHaveLength(1);
+    });
+    it("Should contain the Header component", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find("Header")).toHaveLength(1);
+    });
+    it("Should contain the Login component", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find("Login")).toHaveLength(1);
+    });
+    it("Should contain the Footer component", () => {
+        const wrapper = shallow(<App />);
+        wrapper.update();
+        expect(wrapper.find("Footer")).toHaveLength(1);
+    });
+});
