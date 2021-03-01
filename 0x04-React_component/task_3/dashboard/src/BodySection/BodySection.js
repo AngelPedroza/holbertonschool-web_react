e4c3cac570c9dd423189de090aa4635e1138acb9
Angel Pedroza <angel.pedroza@rappi.com>
2021-02-28 19:57:49 -0500

    [Add] Task_3

---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BodySection extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="bodySection">
+                <h2>{this.props.title}</h2>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default BodySection;
