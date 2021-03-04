54c856d1acb56ef406328d677b89e7345f881404
Angel Pedroza <angel.pedroza@rappi.com>
2021-03-03 19:57:41 -0500

    [Add] Task_1 ammend 2

--- 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -28,26 +28,26 @@ describe("Testing the behavior of Notification component", () => {
     it("menu item is being displayed when displayDrawer is false", () => {
         const wrapper = shallow(<Notifications />);
         wrapper.update();
-        const item = wrapper.find("div.menuItem");
+        const item = wrapper.find("div#menuItem");
         expect(item).toHaveLength(1);
     });
     it("div.Notifications is not being displayed when displayDrawer is false", () => {
         const wrapper = shallow(<Notifications />);
         wrapper.update();
-        const item = wrapper.find("div.Notifications");
+        const item = wrapper.find("div#Notifications");
         expect(item).toHaveLength(0);
     });
     it("menu item is being displayed when displayDrawer is true", () => {
         const wrapper = shallow(<Notifications displayDrawer />);
         wrapper.update();
-        const item = wrapper.find("div.menuItem");
+        const item = wrapper.find("div#menuItem");
         expect(item).toHaveLength(1);
     });
     it("div.Notifications is being displayed when displayDrawer is true", () => {
         const wrapper = shallow(<Notifications displayDrawer />);
         wrapper.update();
         const item = wrapper.find("div#Notifications");
-        expect(item).toHaveLength(0);
+        expect(item).toHaveLength(1);
     });
 
     describe("Notifications with listNotifications", () => {
