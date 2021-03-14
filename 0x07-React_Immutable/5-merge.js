af2b2f4eef13e219740606886138011a65908445
Angel Pedroza <angel.pedroza@rappi.com>
2021-03-14 16:31:08 -0500

    [Add] Task 5 eslint

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,15 +1,15 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-    const p1 = List(page1);
-    const p2 = List(page2);
+  const p1 = List(page1);
+  const p2 = List(page2);
 
-    return p1.concat(p2);
+  return p1.concat(p2);
 }
 
 export function mergeElements(page1, page2) {
-    const p1 = Map(page1);
-    const p2 = Map(page2);
+  const p1 = Map(page1);
+  const p2 = Map(page2);
 
-    return p1.merge(p2);
+  return p1.merge(p2);
 }
