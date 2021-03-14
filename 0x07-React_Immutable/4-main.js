181e3e7fdb81e2f29b3b3035125a4944bc6468b5
Angel Pedroza <angel.pedroza@rappi.com>
2021-03-14 16:03:20 -0500

    [Add] task 4

---
+++ 0x07-React_Immutable/4-main.js
@@ -0,0 +1,6 @@
+import { map, map2 } from './4-mutations';
+
+console.log(`map.get(2): ${map.get('2')}, map2.get(2): ${map2.get('2')}`);
+console.log(`map.get(4): ${map.get('4')}, map2.get(4): ${map2.get('4')}`);
+
+console.log(`map: ${map}, map2: ${map2}`);
