98d2b057e08340ca8e63ce35a659831d9e03086b
Angel Pedroza <angel.pedroza@rappi.com>
2021-03-25 10:50:11 -0500

    [Add] task_0

---
+++ 0x09-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,50 @@
+import {
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+} from '../actions/uiActionTypes';
+
+export const initialState = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {},
+};
+
+export function uiReducer(state = initialState, action){
+    switch (action.type){
+        case DISPLAY_NOTIFICATION_DRAWER: {
+            return {
+                ...state,
+                isNotificationDrawerVisible: true,
+            };
+        }
+        case HIDE_NOTIFICATION_DRAWER: {
+            return {
+                ...state,
+                isNotificationDrawerVisible: false,
+            };
+        }
+        case LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isUserLoggedIn: true,
+            };
+        }
+        case LOGIN_FAILURE: {
+            return {
+                ...state,
+                isUserLoggedIn: false,
+            };
+        }
+        case LOGOUT: {
+            return {
+                ...state,
+                isUserLoggedIn: false,
+            };
+        }
+        default:
+            return state;
+    }
+};
