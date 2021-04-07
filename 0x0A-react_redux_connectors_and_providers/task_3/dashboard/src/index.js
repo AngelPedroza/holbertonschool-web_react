54d896012cf4ed174c697e104d5da6263138d054
Angel Pedroza <angel.pedroza@rappi.com>
2021-04-06 20:33:46 -0500

    [ADD] task 3 test

--- 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import { uiReducer, initialState } from "./reducers/uiReducer";
+import "redux-devtools-extension";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Map } from "immutable";
 
-const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(uiReducer, Map(initialState), composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
